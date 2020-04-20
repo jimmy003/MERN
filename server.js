5122f3aaaff373bc88a9bd777b0ff20b7891f66b
jimmy003 <jimmy003.deguzman@gmail.com>
2020-04-20 15:29:06 +0800

    added .prettierrc

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.use(cors());
 
 //Init Middleware
 app.use(
-	express.json({
-		extended: false,
-	})
+  express.json({
+    extended: false
+  })
 );
 
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -28,16 +28,15 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 //Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-	//Set static folder
-	app.use(express.static('client/build'));
-
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
+  //Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-	console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
